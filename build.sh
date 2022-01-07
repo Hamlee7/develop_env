7fcc8ec13ab223f7e6b7a6e27153fef824125fc0
Liting <liting@thinkit.cn>
2022-01-07 13:42:36 +0800

    upload the dockerfile of devel and release version

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#base version: devtoolset-7-toolchain-centos7:latest
-#base_name=devtoolset-7-toolchain-centos7
+#image_name=centos8_dev
+#iversion=1.1
+image_name=engine_bamp_release
+iversion=1.0
 
-#latest version
-image_name=centos8_dev
-iversion=1.1
+#dockfilename=Dockerfile.devel
+dockerfile=Dockerfile.release
 
-docker build -t ${image_name}:${iversion} .
 
-#docker load -i ${base_name}.tar \
-#&& docker build -t ${image_name}:${iversion} . \
-#&& docker save -o ${image_name}_${iversion}.tar ${image_name}:${iversion} \
-#&& docker run --rm -it -v /home/work/thinkit/gd/TitSpeechEngineServer:/demo/ --name ${image_name}_${iversion} ${image_name}:${iversion} bash
+docker build -t ${image_name}:${iversion} -f ${dockerfile} . \
+&& docker save -o ${image_name}_${iversion}.tar ${image_name}:${iversion}
